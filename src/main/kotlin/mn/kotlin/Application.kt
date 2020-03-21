4d49c9854e89d7d9451a9ea043873af38d2c0f29
mpppk <niboshiporipori@gmail.com>
2020-03-21 12:38:52 +0900

    First commit

---
+++ src/main/kotlin/mn/kotlin/Application.kt
@@ -0,0 +1,14 @@
+package mn.kotlin
+
+import io.micronaut.runtime.Micronaut
+
+object Application {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        Micronaut.build()
+                .packages("mn.kotlin")
+                .mainClass(Application.javaClass)
+                .start()
+    }
+}
