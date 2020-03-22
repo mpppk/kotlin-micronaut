5718e419c3b394d31b3a284cf0a08af004077826
mpppk <niboshiporipori@gmail.com>
2020-03-22 15:31:15 +0900

    Add OAS integration

--- src/main/kotlin/mn/kotlin/Application.kt
+++ src/main/kotlin/mn/kotlin/Application.kt
@@ -1,7 +1,20 @@
 package mn.kotlin
 
 import io.micronaut.runtime.Micronaut
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Contact
+import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.annotations.info.License
 
+@OpenAPIDefinition(
+        info = Info(
+                title = "mn-kotlin",
+                version = "0.0",
+                description = "mn-kotlin api",
+                license = License(name = "Apache 2.0", url = "https://github.com/mpppk/mn-kotlin"),
+                contact = Contact(url = "https://twitter.com/mpppk", name = "niboshi", email = "niboshiporipori@gmail.com")
+        )
+)
 object Application {
 
     @JvmStatic
